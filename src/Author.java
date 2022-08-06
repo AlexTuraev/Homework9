893033e46da95c63e288d3eba2689429e08dfece
Alex Turaev <altex_76@mail.ru>
2022-08-06 07:58:41 +0300

    homework9 is done

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getAuthorName() {
+        return name;
+    }
+
+    public String getAuthorSurname() {
+        return surname;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        /*Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Михаил", "Шолохов");
 
+        Book book1 = new Book("Война и мир", author1, 2015);
+        Book book2 = new Book("Поднятая целина", author2, 1956);*/
     }
 }
